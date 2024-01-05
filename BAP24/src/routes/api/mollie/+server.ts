e70ecc51a5e1a11f65cbf0dc9906beb174e79c82
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2024-01-05 19:48:32 +0100

    debugging

--- BAP24/src/routes/api/mollie/+server.ts
+++ BAP24/src/routes/api/mollie/+server.ts
@@ -18,7 +18,7 @@ export async function POST({ request }) {
           },
           description:  `${surveyName} - ` + userId,
           redirectUrl: 'https://bap24.hosted-power.dev/payment-success',
-          webhookUrl: 'https://bap24.hosted-power.dev/api/mollie/webhook',
+          webhookUrl: 'https://bap24.hosted-power.dev/api/check-payment/:paymentId',
           metadata: {
             userId: PROFILE_ID
           },
