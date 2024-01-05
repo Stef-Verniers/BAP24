49312b98db1515f153a32c2788f585e50b46742b
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2024-01-05 19:09:18 +0100

    debugging

--- BAP24/src/routes/api/check-payment/[id]/server.ts
+++ BAP24/src/routes/api/check-payment/[id]/server.ts
@@ -1,27 +1,24 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const { createMollieClient } = require('@mollie/api-client');
-import { MOLLIE } from "$lib/server/config";
-
-app.use(cors());
+import type { RequestHandler } from '@sveltejs/kit';
+import { createMollieClient } from '@mollie/api-client';
+import { MOLLIE } from "$env/static/private"; // Zorg ervoor dat MOLLIE correct is geïmporteerd
 
 const mollieClient = createMollieClient({ apiKey: MOLLIE });
 
-app.use(express.json());
+console.log('test');
 
-app.get('/api/check-payment/:paymentId', async (req, res) => {
+export const get: RequestHandler = async ({ params }) => {
     try {
-        const paymentId = req.params.paymentId;
+        const paymentId = params.id;
         const payment = await mollieClient.payments.get(paymentId);
-        res.json({ paymentStatus: payment.status });
+        return {
+            status: 200,
+            body: { paymentStatus: payment.status }
+        };
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Er is een fout opgetreden' });
+        return {
+            status: 500,
+            body: { message: 'Er is een fout opgetreden' }
+        };
     }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server luistert op poort ${PORT}`);
-});
+};
