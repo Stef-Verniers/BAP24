49ff356543c872d2f8b0112c079eca8f9e10b6fd
Stef Verniers <stefvern2@student.arteveldehs.be>
2024-01-06 16:27:30 +0100

    debugging

--- BAP24/src/routes/api/mollie/+server.ts
+++ BAP24/src/routes/api/mollie/+server.ts
@@ -1,7 +1,21 @@
 import { json } from "@sveltejs/kit";
 import { createMollieClient }  from "@mollie/api-client";
 import { MOLLIE, PROFILE_ID } from "$lib/server/config";
+import { auth } from '$lib/server/lucia';
 import type { PaymentMethod } from "@mollie/api-client";
+import type { PageServerLoad } from "../../$types";
+import { redirect } from "@sveltejs/kit";
+
+let session;
+let user;
+
+export const load: PageServerLoad = async ({ locals }) => {
+	session = await locals.auth.validate();
+    if (!session) {
+        throw redirect(302, "/");
+    }
+    user = await auth.getUser(session.user.userId)
+}
 
 // Mollie integratie... 
 export async function POST({ request }) {
@@ -20,7 +34,8 @@ export async function POST({ request }) {
           redirectUrl: 'https://bap24.hosted-power.dev/payment-success',
           webhookUrl: 'https://bap24.hosted-power.dev/api/mollie/webhook',
           metadata: {
-            userId: PROFILE_ID
+            userId: PROFILE_ID,
+            currentLoggedInUser: user.userId,
           },
           method: ['ideal', 'bancontact', 'belfius', 'creditcard', 'paypal', 'paysafecard', 'sofort'] as PaymentMethod[],
         });
