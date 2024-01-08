64ff84197db41d4e8e5184f9ee87422417935b59
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2024-01-08 16:35:16 +0100

    cors

--- BAP24/server.js
+++ BAP24/server.js
@@ -2,7 +2,9 @@ import http from 'http';
 import express from 'express';
 import { handler } from './build/handler.js';
 import router from './src/routes/api/mollie/webhook/webhook.js';
+import cors from 'cors';
 
+app.use(cors());
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
