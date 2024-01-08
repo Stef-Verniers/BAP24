5fae00040e2744531b9bad96fa39206de1f25476
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2024-01-08 19:14:53 +0100

    Rebooting

--- BAP24/src/routes/webhook/webhook.js
+++ BAP24/src/routes/webhook/webhook.js
@@ -5,17 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { prisma } from '../../../src/lib/server/prisma.js';
 const router = express.Router();
-
 const mollieClient = createMollieClient({ apiKey: process.env.MOLLIE});
 
 router.post('/', async (req, res) => {
-    console.log('Headers:', req.headers);
-    console.log('Body:', req.body);
     try {
         console.log('Webhook ontvangen na de try', req.body);
         const paymentId = req.body.id;
         const payment = await mollieClient.payments.get(paymentId);
-        console.log('Betaling ontvangen:', payment);
         res.status(200).send('OK');
         if (payment.status === 'paid') {
             console.log('Betaling verwerkt en database bijgewerkt');
