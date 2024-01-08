3f1a4f684238d2be25dfec7df1c6fba966a18d04
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2024-01-08 17:07:13 +0100

    Testing

--- BAP24/src/routes/api/mollie/webhook/webhook.js
+++ BAP24/src/routes/api/mollie/webhook/webhook.js
@@ -9,6 +9,7 @@ const router = express.Router();
 const mollieClient = createMollieClient({ apiKey: process.env.MOLLIE});
 
 router.post('/', async (req, res) => {
+    
     console.log('Webhook ontvangen', req.body);
     try {
         console.log(req.body)
