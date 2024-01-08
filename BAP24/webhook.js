ab702893d65953c2e4f70f1ac2bc2f1f26135710
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2024-01-08 14:46:24 +0100

    webhook change

--- BAP24/webhook.js
+++ BAP24/webhook.js
@@ -1,9 +1,15 @@
-const mollie = require('@mollie/api-client');
-const app = require('express')();
-const bodyParser = require('body-parser');
-require('dotenv').config();
+import createMollieClient from '@mollie/api-client';
+import http from 'http';
+import express from 'express';
+import dotenv from 'dotenv';
 
-const mollieClient = mollie({ apiKey: process.env.MOLLIE_API_KEY });
+const app = express();
+dotenv.config
+
+const server = http.createServer(app);
+const PORT = 3000;
+
+const mollieClient = createMollieClient({ apiKey: process.env.MOLLIE_API_KEY });
 console.log(process.env.MOLLIE_API_KEY);
 
 // app.post((req, res) => {
