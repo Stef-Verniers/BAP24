9746a4a572be394f245c5a8bd206998679c3beeb
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2024-01-08 17:22:22 +0100

    Debugging

--- BAP24/server.js
+++ BAP24/server.js
@@ -10,6 +10,8 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/mollie/webhook', router);
 app.use(handler);
 
--- BAP24/src/routes/api/mollie/webhook/webhook.js
+++ BAP24/src/routes/api/mollie/webhook/webhook.js
@@ -14,7 +14,6 @@ router.post('/', async (req, res) => {
     try {
         console.log('Webhook ontvangen na de try', req.body);
         const paymentId = req.body.id;
-        console.log()
         const payment = await mollieClient.payments.get('tr_FyWE8jC2EN');
 
         console.log('Betaling ontvangen:', payment);
